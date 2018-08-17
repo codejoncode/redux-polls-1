d68bb5e0d8883869549a4ffcea41a4545956b51f
codejoncode <jonathanjholloway@gmail.com>
2018-08-17 18:28:33 -0500

    added the react-redux-loading dependencies. Now a red bar moves across the page to indicate data being loaded in.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,20 +2,24 @@ import {getInitialData} from '../utils/api'
 import {receiveUsers} from '../actions/users'
 import {recievePolls} from '../actions/polls'
 import {setAuthedUser} from '../actions/authedUser'
+import {showLoading, hideLoading} from 'react-redux-loading'
 
 const AUTHED_ID = 'dan_abramov';
 const ERROR = 'ERROR'; 
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData() 
         .then(({users,polls}) => {
             dispatch(receiveUsers(users))
             dispatch(recievePolls(polls))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
         .catch(error => {
             dispatch({type: ERROR});
+            dispatch(hideLoading())
         })
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'; 
 import {handleInitialData} from '../actions/shared'
 import Dashboard from './dashboard.js'; 
-
+import LoadingBar from 'react-redux-loading'
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
@@ -11,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar />
          {this.props.loading === true ? null : <Dashboard />}
       </div>
     )
