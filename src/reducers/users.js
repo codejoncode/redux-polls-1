ba1ceb5a8d29e6c9dc0570e07bc2e927e3885bd1
codejoncode <jonathanjholloway@gmail.com>
2018-08-17 22:38:03 -0500

    the functionality for the user to answer a question that hasn't been answered by the authenticated user has been created.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,6 @@
 import {RECEIVE_USERS} from '../actions/users';
 import {ADD_POLL} from '../actions/polls'
+import {ADD_ANSWER} from '../actions/answers'
 
 export default function users (state = {}, action ) {
     switch(action.type) {
@@ -20,6 +21,16 @@ export default function users (state = {}, action ) {
                 //Michael M. mentioned this today in the one on one.. 8-17-18
                 //the code spreads in the state except for the author
                 //  it spreads in the properties of the author except for the polls in which it wants to change. 
+            }
+        case ADD_ANSWER :
+            const user = state[action.authedUser]
+
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...user,
+                    answers: user.answers.concat([user])
+                }
             }
         default: 
             return state
